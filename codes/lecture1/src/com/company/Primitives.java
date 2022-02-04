2e9798de1aa033fec2ac246bd4d50d8aeea1886b
Ayushi Indu <singhayushi535@gmail.com>
2022-02-05 05:28:53 +0530

    Files added

---
+++ codes/lecture1/src/com/company/Primitives.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largedecimalnumbers = 647347453.644764834;
+        long largeinteger = 738473857395695745L;
+        boolean check = true;
+        //string is not primitive (can be broken down further)
+        String name = "kunal";
+    }
+}
